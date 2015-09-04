2af8dbf32158c2214905ba2826347a7eb9dfbca5
sathish chinniah <sathish73ec@gmail.com>
2015-09-04 20:32:39 +0530

    Initial Commit

---
+++ another exp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  another exp
+//
+//  Created by Sathish Chinniah on 04/09/15.
+//  Copyright (c) 2015 Sathish Chinniah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ another exp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  another exp
+//
+//  Created by Sathish Chinniah on 04/09/15.
+//  Copyright (c) 2015 Sathish Chinniah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
